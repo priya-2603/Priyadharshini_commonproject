3e2b2e58b595764a6fdf68fe170d4f32f48a0c2a
Priyadharshini Narayanan <priyanarayanan2603@gmail.com>
2024-08-06 17:19:47 +0530

    angularfiles

---
+++ Angular/EMS/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(),
+    EmployeeService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
